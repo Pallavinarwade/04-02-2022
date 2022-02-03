724b7b5db91eee5a4a91cb2c8c65a3e8ce156957
PALLAVI <PALLAVI@LAPTOP-BUP8J5B8>
2022-02-03 16:13:47 +0530

    com.javaLiterals package added into project 03-02-2022

---
+++ 03-02-2022/src/com/javaLiterals/Example1.java
@@ -0,0 +1,28 @@
+package com.javaLiterals;
+
+public class Example1 
+{
+
+	public static void main(String[] args) 
+	{
+	//Write a java program to learner information (id, name, age, address) using java literals
+		
+		int learnerId = 4949;
+		String learnerName = "Pallavi";
+		int learnerAge = 21;
+		String learnerAddress = "Hyderabad";
+		
+		System.out.print("learner Id:");
+		System.out.println(learnerId);
+		
+		System.out.print("learner Name");
+		System.out.println("learnerName");
+		
+		System.out.print("learner Age");
+		System.out.println("learnerAge");
+		
+		System.out.print("learner Address");
+		System.out.println("learnerAddress");
+		
+	}
+}
